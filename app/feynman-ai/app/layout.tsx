4cc6bcffaa0454942413e0c32adc9070ae1cec6c
Peter Gelderbloem <petersgelderbloem@gmail.com>
2023-09-01 00:21:48 +0100

    Initial commit of app based on Vercel Precedent template

---
+++ app/feynman-ai/app/layout.tsx
@@ -0,0 +1,38 @@
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import cx from "classnames";
+import { sfPro, inter } from "./fonts";
+import Nav from "@/components/layout/nav";
+import Footer from "@/components/layout/footer";
+import { Suspense } from "react";
+
+export const metadata = {
+  title: "Feynman AI - Check your understanding of important concepts",
+  description:
+    "Feynman AI - Check your understanding of important concepts. Supply concept text and your explanation in your own words and Feynman AI will evaluate your accuracy",
+  metadataBase: new URL("https://precedent.dev"),
+  themeColor: "#FFF",
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={cx(sfPro.variable, inter.variable)}>
+        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
+        <Suspense fallback="...">
+          {/* @ts-expect-error Server Component */}
+          <Nav />
+        </Suspense>
+        <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+          {children}
+        </main>
+        <Footer />
+        <Analytics />
+      </body>
+    </html>
+  );
+}
---
+++ app/feynman-ai/app/page.tsx
@@ -0,0 +1,60 @@
+import Card from "@/components/home/card";
+import Balancer from "react-wrap-balancer";
+import { DEPLOY_URL } from "@/lib/constants";
+import { Github, Twitter } from "@/components/shared/icons";
+import WebVitals from "@/components/home/web-vitals";
+import ComponentGrid from "@/components/home/component-grid";
+import Image from "next/image";
+import { nFormatter } from "@/lib/utils";
+
+export default async function Home() {
+
+  return (
+    <>
+      <div className="z-10 w-full max-w-xl px-5 xl:px-0">
+        <h1
+          className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
+          style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+        >
+          <Balancer>Feynman AI</Balancer>
+        </h1>
+        <p
+          className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
+          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+        >
+          <Balancer>
+            Check your understanding of important concepts
+          </Balancer>
+        </p>
+        <div
+          className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
+          style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
+        >
+
+        </div>
+      </div>
+      <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-4 xl:px-0">
+
+          <Card
+            key={"concept"}
+            title={"concept"}
+            description={"put the concept you want to understand here"}
+            large={false}
+          />
+          <Card
+            key={"yourWords"}
+            title={"in your own words"}
+            description={"put in-your-own words explanation here"}
+            large={false}
+          />
+          <Card
+            key={"evaluation"}
+            title={"Evaluation"}
+            description={"AI evaluation of your understanding"}
+            large={true}
+          />
+
+      </div>
+    </>
+  );
+}
