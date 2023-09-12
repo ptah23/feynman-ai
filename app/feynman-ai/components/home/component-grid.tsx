26b6b1ad3e0bc6c1470ed567cc522a282a9434c3
Peter Gelderbloem <petersgelderbloem@gmail.com>
2023-09-12 23:31:00 +0100

    cleanup

--- app/feynman-ai/components/home/component-grid.tsx
+++ app/feynman-ai/components/home/component-grid.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useState } from "react";
+
 import { useDemoModal } from "@/components/home/demo-modal";
 
-import Tooltip from "@/components/shared/tooltip";
-import { ChevronDown } from "lucide-react";
 
 export default function ComponentGrid() {
   const { DemoModal, setShowDemoModal } = useDemoModal();
@@ -17,12 +15,7 @@ export default function ComponentGrid() {
       >
         <p className="text-gray-600">Modal</p>
       </button>
-
-      <Tooltip content="Precedent is an opinionated collection of components, hooks, and utilities for your Next.js project.">
-        <div className="flex w-36 cursor-default items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100">
-          <p className="text-gray-600">Tooltip</p>
-        </div>
-      </Tooltip>
+        
     </div>
   );
 }
