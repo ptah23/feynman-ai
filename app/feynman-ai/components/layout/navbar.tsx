fc1460c1ff6519f8185e47ac5e8169df2da0b837
Peter Gelderbloem <petersgelderbloem@gmail.com>
2023-09-13 01:12:49 +0100

    remove auth from navbar

--- app/feynman-ai/components/layout/navbar.tsx
+++ app/feynman-ai/components/layout/navbar.tsx
@@ -3,17 +3,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import useScroll from "@/lib/hooks/use-scroll";
-import { useSignInModal } from "./sign-in-modal";
 
-import { Session } from "next-auth";
-
-export default function NavBar({ session }: { session: Session | null }) {
-  const { SignInModal, setShowSignInModal } = useSignInModal();
+export default function NavBar() {
   const scrolled = useScroll(50);
 
   return (
     <>
-      <SignInModal />
       <div
         className={`fixed top-0 w-full flex justify-center ${
           scrolled
@@ -32,21 +27,7 @@ export default function NavBar({ session }: { session: Session | null }) {
             ></Image>
             <p>Feynman AI</p>
           </Link>
-            {/*}
-          <div>
-            {session ? (
-              <UserDropdown session={session} />
-            ) : (
-              <button
-                className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
-                onClick={() => setShowSignInModal(true)}
-              >
-                Sign In
-              </button>
-            )
-          </div>
-          }
-            */}
+            {}
         </div>
       </div>
     </>
