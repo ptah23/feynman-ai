08f569c0ea381a401016cd531d3b162cc353a9b0
Peter Gelderbloem <petersgelderbloem@gmail.com>
2023-09-13 21:33:07 +0100

    add cohere api call

--- app/feynman-ai/api/index.py
+++ app/feynman-ai/api/index.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import cohere
+import os
 
 class EvaluationRequest(BaseModel):
     concept: str
@@ -15,4 +16,21 @@ def hello():
 
 @app.post("/api/evaluate")
 def evaluate(request:EvaluationRequest):
-    return "Evaluation complete"
+    prompt =  '''
+        Read the following concept and a student's in-your-own-words description and evaluate the student's
+        description by the following criteria: 
+        - coverage of the core aspects of the concept
+        - clarity and simplicity of the explanation
+        - Identify gaps in understanding and areas that need improvement
+        
+        Provide a score on how well the concept was explained.
+        
+        Concept: `{concept}`
+        
+        Student: `{explanation}`
+        Evaluation:'''.format(concept=request.concept, explanation=request.explanation)
+    co = cohere.Client(os.environ.get('COHERE_API_KEY'))
+    response = co.generate(
+        prompt=prompt,
+)
+    return response
