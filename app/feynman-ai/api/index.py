a9a9c65171ff159b98ddcdf4ace25a0c1b71f1db
Peter Gelderbloem <petersgelderbloem@gmail.com>
2023-09-14 23:42:31 +0100

    improve prompt

--- app/feynman-ai/api/index.py
+++ app/feynman-ai/api/index.py
@@ -18,21 +18,28 @@ def hello():
 @app.post("/api/evaluate")
 def evaluate(request:EvaluationRequest):
     prompt =  '''
-        Read the following concept prefixed with `Concept` and a student's in-your-own-words explanation prefixed with `Student` and evaluate the student's
-        explanation by the following criteria: 
-        - coverage of the core aspects of the concept
-        - clarity and simplicity of the explanation
-        - Identify gaps in understanding and areas that need improvement
+Act as a criticaleducator expert at giving positive criticism.
+Read the following concept prefixed with `Concept:` and a student's in-your-own-words explanation prefixed with `Student:` and evaluate the student's
+explanation by the following criteria:
+- coverage of the core aspects of the concept.
+- clarity and simplicity of the explanation
+- Identify gaps in understanding and areas that need improvement
+
+Provide a score out between 0 and 10 based on how well the above criteria were met by the Student's explanation.
+
+Only use information from Concept section between the tick marks as the concept. Do not relate it to any of your existing knowledge. Just use the concept stand alone.
+Keep it to three sentences. Do not repeat yourself.
         
-        Provide a score out of 10 on how well the concept was explained.
+Concept: `{concept}`
         
-        Concept: `{concept}`
+Student: `{explanation}`
         
-        Student: `{explanation}`
-        
-        Evaluation:'''.format(concept=request.concept, explanation=request.explanation)
+Evaluation:'''.format(concept=request.concept, explanation=request.explanation)
+    print(prompt)
     request = {
-        "max_tokens": 2000,
+        "model": "command",
+        "max_tokens": 300,
+        "temperature": 0,
         "truncate": "END",
         "return_likelihoods": "NONE",
         "prompt": prompt
@@ -44,5 +51,5 @@ def evaluate(request:EvaluationRequest):
     headers["content-type"] = "application/json"
 
     response = requests.post("https://api.cohere.ai/v1/generate", json=request, headers=headers)
-    print(response.json()["generations"][0]["text"])
+    print(response.json())
     return response.json()["generations"][0]["text"]
